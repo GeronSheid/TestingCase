cb18a05b495fdc9e77836669e53fabb9677883a2
Dmitry Shneider <dmitryshneider@gmail.com>
2023-08-26 16:55:55 +0600

    Added lower gradation

--- src/components/ShadeGrade/ShadeGrade.jsx
+++ src/components/ShadeGrade/ShadeGrade.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-
+import './ShadeGreade.css'
 const ShadeGrade = () => {
     return (
-        <div>
-            <ul>
-                <li>
-
-                </li>
+        <div className='mt'>
+            <ul className='flex-list'>
+                <li className='smaller'>Меньше</li>
+                <li className='zero'></li>
+                <li className='small'></li>
+                <li className='medium'></li>
+                <li className='large'></li>
+                <li className='veryLarge'></li>
+                <li className='bigger'>Больше</li>
             </ul>
         </div>
     );
